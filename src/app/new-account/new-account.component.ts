de5c3d826214aa8605c9677eeeb37393033036a4
Marko Kozjak <kozypato@gmail.com>
2017-12-27 15:41:28 +0100

    Emitting event from a service and subscribing to it in a component

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,13 +12,16 @@ import {AccountsService} from '../accounts.service';
 export class NewAccountComponent {
 
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
-
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService
+  ) {
+  this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New status:' + status)
+  )
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
 
   }
 }
